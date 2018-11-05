335198b626cea09193265f5fb304a8631dfe7efa
Tuomas Karna <tuomas.karna@gmail.com>
2018-11-05 18:27:31 +0200

    update installation instructions

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1276.g4e1ded2',
+    VERSION: '0+untagged.1278.g687ecb8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
