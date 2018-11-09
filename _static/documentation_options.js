89550bfd6e133f3ec2280ab27582ee46988a3ed4
Tuomas Karna <tuomas.karna@gmail.com>
2018-11-09 15:58:28 +0200

    fix https links

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1278.g687ecb8',
+    VERSION: '0+untagged.1284.g0b23eeb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
