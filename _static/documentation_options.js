afdaaf6935d5f10958d8cbc4f7f4eec2f6626095
Tuomas Karna <tuomas.karna@gmail.com>
2020-08-04 15:12:39 +0300

    update publications list

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1409.g7b98a42',
+    VERSION: '0+untagged.1562.gc08033a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
