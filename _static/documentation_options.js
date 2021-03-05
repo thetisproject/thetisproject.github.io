172812ad16296b5e855ee1af51407f39861374e5
jwallwork23 <j.wallwork16@imperial.ac.uk>
2021-03-05 17:05:36 +0000

    Revert to master

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1653.ged98105.dirty',
+    VERSION: '0+untagged.1652.g469c17b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
