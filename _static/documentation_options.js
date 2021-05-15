9fae0c20e045e0162bb21cda8da01cee8d24f15c
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-05-15 10:18:56 +0100

    Update in line with PR #240

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1675.g2c5c7fa',
+    VERSION: '0+untagged.1676.g1d09a98',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
