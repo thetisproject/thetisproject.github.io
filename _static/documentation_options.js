84a3140e49c379a1e02ad690e8ebbb118eea4da8
Tuomas Karna <tuomas.karna@gmail.com>
2021-06-16 12:13:19 +0300

    add Bateman et al. (2021) paper

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1676.g1d09a98',
+    VERSION: '0+untagged.1689.ge390b9e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
