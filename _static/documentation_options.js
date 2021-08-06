253267b312d7c74eab5008fbd3d747949c6f53c6
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-08-06 13:13:52 +0100

    Update following PR258

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1720.g734247e',
+    VERSION: '0+untagged.1736.gc23b50a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
